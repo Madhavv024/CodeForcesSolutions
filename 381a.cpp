545da1e644d74fe44bcf2f5b84e234311513f6c9
Madhavv024 <madhavvarjuntthukral@gmail.com>
2024-09-16 22:02:56 +0530

    Revert "CF solutions added for 1399a , 1409a , 381a"
    This reverts commit eb0fad23b1b6d0fa74c8fa4d6f06d7a2007cebdb.

--- 381a.cpp
+++ 381a.cpp
@@ -24,29 +24,13 @@ int32_t main()
         for(int i = 0;i<n;i++){
             cin>>arr[i];
         }
-        int s = 0 , d = 0 , l = 0 , r = n-1 , turn = 1;
-        while(l<=r){
-            if(turn){
-                if(arr[l]>arr[r]){
-                    s+= arr[l];
-                    l++;
-                }else{
-                    s+=arr[r]; 
-                    r--;
-                }
-                turn = 0;
-            }else{
-                if(arr[l]>arr[r]){
-                    d+= arr[l];
-                    l++;
-                }else{
-                    d+=arr[r]; 
-                    r--;
-                }
-                turn = 1;
+        int s = 0 , d = 0 , l = 0 , r = n-1;
+        int take = n, turn = 0;
+        while(take!=0){
+            if(turn%2==0){
+                if(a[])
             }
         }
-        cout<<s<<" "<<d<<"\n";
     }
     return 0;
 }
